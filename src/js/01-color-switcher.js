d33db4547d9eb487105c2372b6c1cef9a4a1b140
Maryna-Korbet <korbet.maryna.developer@gmail.com>
2023-11-21 19:19:43 +0200

    Added color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const body = document.querySelector('body');
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+
+let timerId = null;
+
+startBtn.addEventListener('click', startTimer);
+stopBtn.addEventListener('click', stopTimer);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function backgroundColorSwitcher() {
+    const newColor = getRandomHexColor();
+    body.style.backgroundColor = newColor;
+}
+
+function startTimer() {
+    timerId = setInterval(backgroundColorSwitcher, 1000);
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+}
+
+function stopTimer() {
+    clearInterval(timerId);
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+}
 
